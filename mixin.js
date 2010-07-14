317874eb914a49ddc2bebaea1e462e088b7312f7
Joel Rosario <cirquitz@gmail.com>
2010-07-15 00:58:02 +0530

    mixin.mix now returns the context object passed in.

--- mixin.js
+++ mixin.js
@@ -8,4 +8,6 @@ exports['mix'] = function (filename, context) {
   context['exports'] = exports;
   for(var key in global) { context[key] = global[key]; }
   Script.runInNewContext(data, context, filename);
+
+  return context;
 };
